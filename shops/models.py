2a7ecfd0177e1177eede2253e7a16f2f269dfe74
Nikita <nik.matue@gmail.com>
2018-07-28 20:01:32 +0500

    beginning

--- shops/models.py
+++ shops/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from products.models import CommonModel
 
-# Create your models here.
+
+class Shop(CommonModel):
+    shop_name = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return '%s' % self.shop_name
+
+    class Meta:
+        verbose_name = 'Магазин'
+        verbose_name_plural = 'Магазины'
+
