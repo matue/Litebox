7685dc2a1e7dabf93badfdb1409f4e6aa23741de
Nikita <nik.matue@gmail.com>
2018-07-28 20:36:49 +0500

    сериализаторы, админка, модели

--- docs/admin.py
+++ docs/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class ProductsInDocInline(admin.TabularInline):  # добавляем в админку возможность указывать товары в документе
+    model = ProductsInDoc
+    extra = 0  # дополнительные ряды для добавления
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Document._meta.fields]
+    inlines = [ProductsInDocInline]
+
+
+admin.site.register(DocType)
+admin.site.register(Document, DocumentAdmin)
--- docs/models.py
+++ docs/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from products.models import CommonModel, Product
+from shops.models import Shop
 
-# Create your models here.
+
+class DocType(CommonModel):
+    doc_type_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return '%s' % self.doc_type_name
+
+    class Meta:
+        verbose_name = 'Тип документа'
+        verbose_name_plural = 'Типы документов'
+
+
+class Document(CommonModel):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, default=None)
+    doc_type = models.ForeignKey(DocType, on_delete=models.CASCADE, default=None)
+    # person = models.ForeignKey(Person, on_delete=models.CASCADE, default='1', blank=True, null=True)
+    date = models.DateField('Date', default=None)
+
+    def __str__(self):
+        return 'Дата: %s, Магазин: %s' % (self.date, self.shop.shop_name)
+
+    class Meta:
+        verbose_name = 'Документ'
+        verbose_name_plural = 'Документы'
+
+
+class ProductsInDoc(CommonModel):  # промежуточная модель, связывает продукты и документы для указания количества продуктов в документе
+    document = models.ForeignKey(Document, on_delete=models.CASCADE, default=None)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1, null=False, blank=False)
+
+    def __str__(self):
+        return 'Продукт: %s, Документ: %s' % (self.product.product_name, self.document.doc_type)
+
+    class Meta:
+        verbose_name = 'Запись в документе'
+        verbose_name_plural = 'Записи в документе'
